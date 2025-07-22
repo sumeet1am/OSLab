7ccefbcd5b5bc809ced6aed917224dd69a9754a5
sumeet1am <sumeetam07@gmail.com>
2025-07-22 21:40:31 +0530

    Update and rename preemptive.c to SJFpreemptive.c

--- q01.c/SJFpreemptive.c
+++ q01.c/SJFpreemptive.c
@@ -4,13 +4,15 @@
 #define MAX 100
 
 typedef struct {
-    int id, at, bt, rt, st, ct, tat, wt, started;
+    int id, at, bt, rt;
+    int st, ct, tat, wt;
+    int started;
 } Process;
 
 void input(Process p[], int n) {
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         p[i].id = i + 1;
-        printf("Enter Arrival Time and Burst Time of P%d: ", p[i].id);
+        printf("Enter Arrival Time and Burst Time for P%d: ", p[i].id);
         scanf("%d%d", &p[i].at, &p[i].bt);
         p[i].rt = p[i].bt;
         p[i].started = 0;
@@ -18,18 +20,18 @@ void input(Process p[], int n) {
 }
 
 int findShortest(Process p[], int n, int time) {
-    int min_time = INT_MAX, index = -1;
-    for(int i = 0; i < n; i++) {
-        if (p[i].at <= time && p[i].rt > 0 && p[i].rt < min_time) {
-            min_time = p[i].rt;
-            index = i;
+    int idx = -1, min_rt = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        if (p[i].at <= time && p[i].rt > 0 && p[i].rt < min_rt) {
+            min_rt = p[i].rt;
+            idx = i;
         }
     }
-    return index;
+    return idx;
 }
 
 void calculate(Process p[], int n) {
-    int time = 0, completed = 0;
+    int time = 0, completed = 0, prev = -1;
     float total_tat = 0, total_wt = 0;
 
     printf("\nGantt Chart:\n");
@@ -47,7 +49,13 @@ void calculate(Process p[], int n) {
             p[idx].started = 1;
         }
 
-        printf("| P%d ", p[idx].id);
+        // Print only on context switch
+        if (idx != prev) {
+            printf("| P%d ", p[idx].id);
+            prev = idx;
+        }
+
+        // Execute for 1 unit
         p[idx].rt--;
         time++;
 
@@ -62,9 +70,10 @@ void calculate(Process p[], int n) {
     }
 
     printf("|\n\nProcess\tAT\tBT\tST\tCT\tTAT\tWT\n");
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         printf("P%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
-            p[i].id, p[i].at, p[i].bt, p[i].st, p[i].ct, p[i].tat, p[i].wt);
+               p[i].id, p[i].at, p[i].bt, p[i].st,
+               p[i].ct, p[i].tat, p[i].wt);
     }
 
     printf("\nAverage Turnaround Time: %.2f", total_tat / n);
