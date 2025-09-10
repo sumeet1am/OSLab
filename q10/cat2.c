b24324db1202b1648eba8d67f671b8486b86fcf1
Sumeet Mahalingapur <sumeetam1@gmail.com>
2025-09-10 21:41:02 +0530

    Update cat2.c

--- q10/cat2.c
+++ q10/cat2.c
@@ -1,22 +1,30 @@
-#include<stdio.h> 
-#include<stdlib.h> 
-void main(int argc, char *argv[]) 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) 
 { 
-FILE *fp; 
-char ch; 
-if(argc<2) 
-{ 
-} 
-printf("File not entered"); 
-exit(1); 
-fp=fopen(argv[1],"r"); 
-if(fp==NULL) 
-{ 
-} 
-printf("File does not exist\n"); 
-exit(1); 
-while((ch=fgetc(fp))!=EOF) 
-printf("%c",ch);  
-fclose(fp); 
-printf("\n");   
+    FILE *fp; 
+    char ch; 
+
+    if (argc < 2)  // No file argument given
+    { 
+        printf("File not entered\n"); 
+        exit(1); 
+    } 
+
+    fp = fopen(argv[1], "r"); 
+    if (fp == NULL)  // File not found
+    { 
+        printf("File does not exist\n"); 
+        exit(1); 
+    } 
+
+    // Read and print file contents
+    while ((ch = fgetc(fp)) != EOF) 
+        printf("%c", ch);  
+
+    fclose(fp); 
+    printf("\n");   
+    
+    return 0; 
 }
