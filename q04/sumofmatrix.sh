16235a1150482dce7aa19cc5e29961fbc6d95c33
Sumeet Mahalingapur <sumeetam1@gmail.com>
2025-09-11 03:05:21 +0530

    Update sumofmatrix.sh

--- q04/sumofmatrix.sh
+++ q04/sumofmatrix.sh
@@ -1,62 +1,60 @@
 #!/bin/bash
 
-# Read dimensions
-echo -n "Enter number of rows: "
-read rows
-echo -n "Enter number of columns: "
-read cols
+# Read size
+echo "Enter rows:"
+read r
+echo "Enter cols:"
+read c
 
 # Read first matrix
-echo "Enter elements of first matrix ($rows x $cols):"
-for ((i=0; i<rows; i++))
+echo "Enter first matrix:"
+for ((i=0;i<r*c;i++))
 do
-    for ((j=0; j<cols; j++))
-    do
-        read a[$((i*cols + j))]
-    done
+  read a[$i]
 done
 
 # Read second matrix
-echo "Enter elements of second matrix ($rows x $cols):"
-for ((i=0; i<rows; i++))
+echo "Enter second matrix:"
+for ((i=0;i<r*c;i++))
 do
-    for ((j=0; j<cols; j++))
-    do
-        read b[$((i*cols + j))]
-    done
+  read b[$i]
 done
 
-# Display first matrix
-echo "First Matrix:"
-for ((i=0; i<rows; i++))
+# Print first matrix
+echo "First matrix:"
+k=0
+for ((i=0;i<r;i++))
 do
-    for ((j=0; j<cols; j++))
-    do
-        echo -n "${a[$((i*cols + j))]} "
-    done
-    echo
+  for ((j=0;j<c;j++))
+  do
+    echo -n "${a[$k]} "
+    ((k++))
+  done
+  echo
 done
 
-# Display second matrix
-echo "Second Matrix:"
-for ((i=0; i<rows; i++))
+# Print second matrix
+echo "Second matrix:"
+k=0
+for ((i=0;i<r;i++))
 do
-    for ((j=0; j<cols; j++))
-    do
-        echo -n "${b[$((i*cols + j))]} "
-    done
-    echo
+  for ((j=0;j<c;j++))
+  do
+    echo -n "${b[$k]} "
+    ((k++))
+  done
+  echo
 done
 
-# Compute and display sum
-echo "Resultant Matrix (Sum):"
-for ((i=0; i<rows; i++))
+# Print sum matrix
+echo "Sum matrix:"
+k=0
+for ((i=0;i<r;i++))
 do
-    for ((j=0; j<cols; j++))
-    do
-        idx=$((i*cols + j))
-        sum=$(( a[idx] + b[idx] ))
-        echo -n "$sum "
-    done
-    echo
+  for ((j=0;j<c;j++))
+  do
+    echo -n "$(( ${a[$k]} + ${b[$k]} )) "
+    ((k++))
+  done
+  echo
 done
